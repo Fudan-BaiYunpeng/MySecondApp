3900a898b5628e7e77651e150a60747b5abec0b6
Fudan-BaiYunpeng <21210240111@m.fudan.edu.cn>
2021-11-01 19:25:57 +0800

    search

--- app/src/main/java/com/example/mysecondapp/SearchActivity.java
+++ app/src/main/java/com/example/mysecondapp/SearchActivity.java
@@ -1,14 +1,33 @@
 package com.example.mysecondapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SearchActivity extends AppCompatActivity {
+
+    private RecyclerView mRecycleView;
+    private SearchAdapter mSearchAdapter = new SearchAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        mRecycleView = findViewById(R.id.rv);
+        mRecycleView.setLayoutManager((new LinearLayoutManager(this)));
+        mRecycleView.setAdapter(mSearchAdapter);
+
+        List<String> items = new ArrayList<>();
+        for (int i = 0; i < 100; i ++){
+            items.add(String.valueOf(i));
+        }
+        mSearchAdapter.notifyItems(items);
     }
 }
---
+++ app/src/main/java/com/example/mysecondapp/TextViewHolder.java
@@ -0,0 +1,28 @@
+package com.example.mysecondapp;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TextViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    private TextView mTextView;
+
+    public TextViewHolder(View itemView){
+        super(itemView);
+        mTextView = itemView.findViewById(R.id.tw);
+        itemView.setOnClickListener(this);
+    }
+
+    public void bind(String text){
+        mTextView.setText(text);
+    }
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(v.getContext(), MainActivity.class);
+        intent.putExtra("extra", mTextView.getText().toString());
+        v.getContext().startActivity(intent);
+    }
+}
