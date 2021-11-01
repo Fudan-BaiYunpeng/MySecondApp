7ae3b389bfced89249e93be774aa205833e86dd0
Fudan-BaiYunpeng <21210240111@m.fudan.edu.cn>
2021-11-01 20:24:58 +0800

    shark

--- app/src/main/java/com/example/mysecondapp/MainActivity.java
+++ app/src/main/java/com/example/mysecondapp/MainActivity.java
@@ -8,9 +8,13 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    private String[] searcoh = {"hhhhh","cccccc","wwwwww","ddddd","aaaaa"};
+//    private String[] searcoh = {"hhhhh","cccccc","wwwwww","ddddd","aaaaa"};
+    private List<String> searcoh = new ArrayList<>();
     private SearchView main_searchview;
     private ListView main_listview;
 
@@ -18,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for (int i = 0; i < 100; i ++){
+//            String s = "这里是第" + String.valueOf(i) + '行';
+            String s = String.valueOf(i);
+            searcoh.add(s);
+        }
+        findViews();
     }
 
     private void findViews(){
@@ -34,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onQueryTextChange(String newText) {
                 if (!TextUtils.isEmpty(newText)){
+//                    newText = "这里是第" + newText + '行';
                     main_listview.setFilterText(newText);
                 }   else {
                     main_listview.clearChoices();
