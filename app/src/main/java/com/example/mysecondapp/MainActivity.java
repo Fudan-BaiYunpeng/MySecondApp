c1223b32ad8520f7518d6ee6d943188b3b4218d7
Fudan-BaiYunpeng <21210240111@m.fudan.edu.cn>
2021-11-01 19:46:42 +0800

    tiger

--- app/src/main/java/com/example/mysecondapp/MainActivity.java
+++ app/src/main/java/com/example/mysecondapp/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.mysecondapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] searcoh = {"hhhhh","cccccc","wwwwww","ddddd","aaaaa"};
+    private SearchView main_searchview;
+    private ListView main_listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void findViews(){
+        main_searchview = (SearchView) findViewById(R.id.main_searchview);
+        main_listview = (ListView) findViewById(R.id.main_listview);
+        main_listview.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, searcoh));
+        main_listview.setTextFilterEnabled(true);
+        main_searchview.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (!TextUtils.isEmpty(newText)){
+                    main_listview.setFilterText(newText);
+                }   else {
+                    main_listview.clearChoices();
+                }
+                return false;
+            }
+        });
     }
 }
